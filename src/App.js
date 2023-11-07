00bb9a86ff2319c21efdaac5d1017d67ea332780
Vamiq Valji <vamiqvalji@vamiqs-air.home>
2023-11-06 23:24:51 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -10,18 +10,16 @@ import {
   theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
+      <Box textAlign="center" fontSize="xl"> 
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
             <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+              GDSC WEBSITE 🎉
             </Text>
             <Link
               color="teal.500"
