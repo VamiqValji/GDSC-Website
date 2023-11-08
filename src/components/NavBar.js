e06389eab90ccd6394a50aa0f22cc63f15aeeaae
Vamiq Valji <vamiqvalji@vamiqs-air.home>
2023-11-08 13:07:44 -0500

    added some starting components

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+
+export const NavBar = ({}) => {
+    return (
+        <>
+            <Text>nav</Text>
+        </>
+    )
+    
+}
