379ed0b3a8ed1da7df5cb6ea481dca7022ec1216
Justin Abuyuan <abuyuanjustin@gmail.com>
2023-11-15 06:50:24 -0500

    Added member: Justin Abuyuan to team page

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -5,6 +5,7 @@ import { MemberCard } from '../components/MemberCard';
 export const Team = ({}) => {
     const members = [
         { name: "Vamiq Valji", linkedIn: "https://www.linkedin.com/in/vamiq-valji/", pfp: "https://media.licdn.com/dms/image/C4D03AQEfPNdqmfUCiw/profile-displayphoto-shrink_800_800/0/1596847659331?e=1705536000&v=beta&t=qeRMFrVOmeSfyUsauAOQIAELVWnQ25rnv3r-mdSn7uk", position: "Tech Lead"},
+        { name: "Justin Abuyuan", linkedIn: "https://www.linkedin.com/in/justin-abuyuan/", pfp: "https://media.licdn.com/dms/image/D5603AQFA39Ep6OyU6Q/profile-displayphoto-shrink_800_800/0/1697921469015?e=1705536000&v=beta&t=2ARUlBpwPf1iWd8LicFaNwyqYbOxFod9K_Ecvvojt2I", position: "Tech Co-Lead"}
     ];
 
     return (
