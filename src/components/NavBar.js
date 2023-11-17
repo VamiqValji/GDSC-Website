ae59d4535b022884f4ca5a75f28bc1edee5419d4
sadasiannerd <106918826+sadasiannerd@users.noreply.github.com>
2023-11-17 00:50:49 -0500

    "Refactor section component in the home page (turn all section1,2,3 components into 1 section component) and navbar component"

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,14 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const NavBar = ({}) => {
     return (
-        <>
-            <nav>
-                <ul>
-                    <li>
+        <Flex minWidth='max-content' alignItems='center' gap='20'>
                         <Link to="/">Home</Link>
-                    </li>
-                    <li>
                         <Link to="/team">Team</Link>
-                    </li>
-                    <li>
                         <Link to="/events">Events</Link>
-                    </li>
-                    <li>
                         <Link to="/learn">Learn</Link>
-                    </li>
-                </ul>
-            </nav>
-        </>
+        </Flex>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,30 @@
 import { Box, Text } from '@chakra-ui/react'
 import React from 'react'
-import { Section1 } from '../components/Section1'
-import { Section2 } from '../components/Section2'
-import { Section3 } from '../components/Section3'
+import { Section } from '../components/Section'
 import { Testimonials } from '../components/Testimonials'
 import { Landing } from '../components/Landing'
+
+const Sections = [
+{
+    Header: "Code.",
+    Content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+},
+{
+    Header: "Create.",
+    Content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+},
+{
+    Header: "Collaborate.",
+    Content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}
+]
 
 export const Home = ({}) => {
     return (
         <>
             <Text>Home</Text>
             <Landing />
-            <Section1 />
-            <Section2 />
-            <Section3 />
+            {Sections.map((section,index) => (<Section key={index} float={index%2===0?"right":"left"} header={section.Header} content={section.Content} picUrl="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpethelpful.com%2Fdogs%2FWhy-Pitbull-Dog-is-Banned-In-25-Countries&psig=AOvVaw0f3UuQSU1bY_W4gXsxY6QE&ust=1700285330291000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMC62sqmyoIDFQAAAAAdAAAAABAJ"/>))}
             <Testimonials />
         </>
     )
