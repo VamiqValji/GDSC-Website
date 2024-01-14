762c57a7363fce39e0deb2172bdef07ac5ca38d9
VamiqValji <vamiq.valji@gmail.com>
2024-01-14 18:27:11 -0500

    started testimonials page

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,10 +1,14 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 
 export const Testimonials = ({}) => {
     return (
-        <>
-            <Text>Testimonials</Text>
-        </>
+        <Flex flexDir="column" justifyContent={"center"} textAlign={"center"}>
+            <Text>See what others have to say</Text>
+            <Text fontSize={{ base: '4xl', sm: '5xl', lg: '6xl' }} fontWeight={"bold"}>Testimonials</Text>
+            <Flex>
+                {/* carousel */}
+            </Flex>
+        </Flex>
     )
 }
