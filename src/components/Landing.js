c32b7e197e95b77ac4086370978a74c956ba13af
VamiqValji <vamiq.valji@gmail.com>
2024-01-14 17:48:14 -0500

    add text to home page landing part

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import { GDSCLogoView } from './GDSCLogoView';
-import { Grid, GridItem, Box } from '@chakra-ui/react';
+import { Grid, GridItem, Box, Flex, Text } from '@chakra-ui/react';
+import colors from "../GoogleColors.json";
 
 export const Landing = ({}) => {
     return (
-            // 3D logo viewport
-            <Grid templateColumns='repeat(2, 1fr)' templateRows='repeat(2, 1fr)' gap={4}>
-                <GridItem rowSpan={2} colSpan={1}> ** Put Landing Text Here ** </GridItem>
-                <GridItem rowSpan={2} colSpan={1}> <Box> <GDSCLogoView/></Box></GridItem>
-            </Grid>
-            
-            
+        <Grid templateColumns='repeat(2, 1fr)' templateRows='repeat(2, 1fr)' gap={4}>
+            <GridItem rowSpan={2} colSpan={1}>
+                <Flex flexDir={"column"} height="100%" justifyContent={"center"}>
+                    <Text color={colors.red500} fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }} fontWeight={"bold"}>CODE.</Text>
+                    <Text color={colors.green500} fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }} fontWeight={"bold"}>CREATE.</Text>
+                    <Text color={colors.blue500} fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }} fontWeight={"bold"}>COLLABORATE.</Text>
+                    <Flex flexDir={"column"} mt={4}>
+                        <Text>Google Developer Student Clubs are community groups for college and university students interested in Google developer technologies.</Text>
+                        <Text color={'gray.500'} mt={3}>Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a GDSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community.</Text>
+                    </Flex>
+                </Flex>
+            </GridItem>
+            {/* // 3D logo viewport */}
+            <GridItem rowSpan={2} colSpan={1}> <Box> <GDSCLogoView/></Box></GridItem>
+        </Grid>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,6 @@ const Sections = [
 export const Home = ({}) => {
     return (
         <Container maxW={'7xl'}>
-            <Text>Home</Text>
             <Landing />
             {Sections.map((section,index) => (<Section key={index} float={index%2===0?"right":"left"} header={section.Header} content={section.Content} picUrl="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpethelpful.com%2Fdogs%2FWhy-Pitbull-Dog-is-Banned-In-25-Countries&psig=AOvVaw0f3UuQSU1bY_W4gXsxY6QE&ust=1700285330291000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMC62sqmyoIDFQAAAAAdAAAAABAJ"/>))}
             <Testimonials />
