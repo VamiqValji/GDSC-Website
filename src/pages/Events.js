53dfea377f18a62304bb0985eeea80d76f6608a3
Justin Abuyuan <abuyuanjustin@gmail.com>
2024-01-25 04:15:40 -0500

    Events page - Adjusted heading and added image placeholder

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,27 +1,45 @@
-import { Container, Heading, Stack, Text } from '@chakra-ui/react'
+import { Container, Heading, Stack, Text, Box } from '@chakra-ui/react'
 import React from 'react'
 import { Section } from '../components/Section'
 
 export const Events = ({}) => {
     const Head = () => (
         <>
-            <Stack direction='column'>
-                <Heading                    
-                        lineHeight={1.1}
-                        fontWeight={600}
-                        fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
-                            <Text>Events</Text>
-                    </Heading>
-                    <br />
-                    <Text color={'gray.500'}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Aliquam elementum odio vitae semper vulputate. Mauris condimentum non justo vitae tempor. 
-                        Duis eu lorem sem. Sed malesuada suscipit elit a rutrum. Sed porttitor vel ligula et sollicitudin. 
-                        Fusce ac nibh euismod felis suscipit tempor. In egestas porta neque et molestie. Quisque malesuada tincidunt ex, 
-                        vel tempus enim sagittis quis. Nulla sodales, quam sit amet interdum egestas, ex sapien volutpat purus, 
-                        non sollicitudin sem dolor et urna.
-                    </Text>
-            </Stack>
+            <Container maxW={'7xl'}>
+                <Stack direction='row-reverse' align={'center'}>
+                    <Box mx={{base: 8}}>
+                        <Stack direction='column' spacing={{ base: 5, md: 10 }}>
+                            <Heading
+                                lineHeight={1.1}
+                                fontWeight={600}
+                                fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}
+                                my={{base: '2'}}
+                            >
+                            <Text as={'span'} position={'relative'}>
+                                Events
+                            </Text>
+                            <br />
+                            </Heading>
+                            <Text color={'gray.500'}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Aliquam elementum odio vitae semper vulputate. Mauris condimentum non justo vitae tempor. 
+                                Duis eu lorem sem. Sed malesuada suscipit elit a rutrum. Sed porttitor vel ligula et sollicitudin. 
+                                Fusce ac nibh euismod felis suscipit tempor. In egestas porta neque et molestie. Quisque malesuada tincidunt ex, 
+                                vel tempus enim sagittis quis. Nulla sodales, quam sit amet interdum egestas, ex sapien volutpat purus, 
+                                non sollicitudin sem dolor et urna.
+                            </Text>
+                        </Stack>
+                    </Box>
+                    <Box
+                        position={'relative'}
+                        height={'330px'}
+                        rounded={'2xl'}
+                        boxShadow={'2xl'}
+                        width={'full'}
+                        overflow={'hidden'}>
+                    </Box>
+                </Stack>
+            </Container>
         </>
     )
 
